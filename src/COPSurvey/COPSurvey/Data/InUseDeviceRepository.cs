e0c069c22a7358b2996329c87771bba220516b7e
sandspd <sandspd@upmc.edu>
2017-07-18 15:13:00 -0400

    Added 'InUseDeviceRepository' class and database file to Data folder.

---
+++ src/COPSurvey/COPSurvey/Data/InUseDeviceRepository.cs
@@ -0,0 +1,21 @@
+﻿using COPSurvey.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace COPSurvey.Data
+{
+    public class InUseDeviceRepository
+    {
+        private static InUseDevice[] _inUseDevicesArray = new InUseDevice[0];
+
+        
+
+        public InUseDevice GetInUseDevice()
+        {
+            return null;
+        }
+
+    }
+}
