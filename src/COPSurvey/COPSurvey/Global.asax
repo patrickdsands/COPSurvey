6977b15fb48eba8100468fa571c8c48997689700
sandspd <sandspd@upmc.edu>
2017-07-18 11:51:06 -0400

    Changed layout of Index.cshtml.

---
+++ src/COPSurvey/COPSurvey/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="COPSurvey.MvcApplication" Language="C#" %>
---
+++ src/COPSurvey/COPSurvey/Models/InUseDevice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace COPSurvey.Models
+{
+    public class InUseDevice
+    {
+        public string SerialNumber { get; set; }
+        public string PCName { get; set; }
+
+        public InUseDevice()
+        {
+            SerialNumber = "";
+            PCName = "";
+        }
+
+        public InUseDevice(string SerialNumber)
+        {
+            this.SerialNumber = SerialNumber;
+        }
+
+    }
+}
