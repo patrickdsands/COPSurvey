a4afc758475dbd1b736d8c68e9d5b26008e334ba
sandspd <sandspd@upmc.edu>
2017-07-18 16:09:10 -0400

    Edited GetInUseDevice method in InUseDeviceRepository method.

--- src/COPSurvey/COPSurvey/Data/InUseDeviceRepository.cs
+++ src/COPSurvey/COPSurvey/Data/InUseDeviceRepository.cs
@@ -1,6 +1,7 @@
 ﻿using COPSurvey.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,20 @@ namespace COPSurvey.Data
 {
     public class InUseDeviceRepository
     {
-        private static InUseDevice[] _inUseDevicesArray = new InUseDevice[0];
+        private static InUseDevice[] _inUseDevices = new InUseDevice[0];
 
-        
+        DataTable inUseDeviceTable = new AMCPMDBPRODDataSet.InUseDeviceDataTable();
 
-        public InUseDevice GetInUseDevice()
+        List<string> listOfSerialNumbers = new List<string>();
+
+        public InUseDeviceRepository()
         {
+            
+        }
+
+        public InUseDevice GetInUseDevice(string SerialNumber)
+        {
+            DataRow[] row = inUseDeviceTable.Select("SN = " + SerialNumber);
             return null;
         }
 
